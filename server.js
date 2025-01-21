be9029ca3e1c8967d9b2376f481767f95471700b
kotdm1996 <dima.kotlarsky@gmail.com>
2025-01-21 00:00:13 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.listen(8080, () => {
-  console.log("Server running on port 8080");
+  console.log("Server running on port ===> 8080");
 });
 
 app.get("/health", (req, res, next) => {
